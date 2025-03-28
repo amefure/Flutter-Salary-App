0f6dfb575ec4d4d620123af0a8c3b42624939855
amefure <shachi10969@gmail.com>
2025-03-28 21:46:34 +0900

    テーマカラーの変更

--- lib/utilitys/custom_colors.dart
+++ lib/utilitys/custom_colors.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 class CustomColors {
   // テキストカラー
   static const Color text = Color(0xFF333333); 
-  static const Color thema = Color(0xFFB12729); 
+  // テーマカラー
+  static const Color thema = Color(0xFF276bb1); 
+
   static const Color foundation = Color(0xFFF2F2F7); 
 }
