36c0649e0702d30bf0da188407d81d2ffcce3ae6
amefure <shachi10969@gmail.com>
2025-03-29 23:12:51 +0900

    テーマカラーバリエーションを定義

---
+++ lib/models/thema_color.dart
@@ -0,0 +1,32 @@
+// フォントサイズのサイズを管理するEnum
+import 'package:flutter/material.dart';
+import 'package:salary/utilitys/custom_colors.dart';
+
+enum ThemaColor {
+  red(CustomColors.negative, 0, "クリムゾンレッド"),
+  blue(CustomColors.thema, 1, "オーシャンブルー"),
+  green(CustomColors.themaGreen, 2, "エメラルドグリーン"),
+  purple(CustomColors.themaPurple, 3, "ロイヤルパープル"),
+  yellow(CustomColors.themaYellow, 4, "サンフラワーイエロー"),
+  orange(CustomColors.themaOrange, 5, "タンジェリンオレンジ"),
+  black(CustomColors.themaBlack, 6, "チャコールブラック"),
+  gray(CustomColors.themaGray, 7, "ストーングレー");
+
+  final Color color;
+  final int value;
+  final String displayName; // 色の名称を追加
+
+  const ThemaColor(this.color, this.value, this.displayName);
+
+  Color toColor() => color;
+  int toValue() => value;
+  String toName() => displayName; // 名称を取得するメソッド
+
+  // int から ThemaColor を取得
+  static ThemaColor fromValue(int value) {
+    return ThemaColor.values.firstWhere(
+      (e) => e.value == value,
+      orElse: () => ThemaColor.blue, // デフォルト
+    );
+  }
+}
--- lib/utilitys/custom_colors.dart
+++ lib/utilitys/custom_colors.dart
@@ -3,11 +3,34 @@ import 'package:flutter/material.dart';
 /// カスタムカラーを定義
 class CustomColors {
   /// テキストカラー
-  static const Color text = Color(0xFF333333); 
-  /// テーマカラー
-  static const Color thema = Color(0xFF276bb1); 
+  static const Color text = Color(0xFF333333);
+
+  /// テーマ(ベース)カラー
+  static const Color thema = Color(0xFF276bb1);
+
   /// ファンデーションカラー
-  static const Color foundation = Color(0xFFF2F2F7); 
+  static const Color foundation = Color(0xFFF2F2F7);
+
   /// ネガティブ
-  static const Color negative = Color(0xFFB12729); 
+  static const Color negative = Color(0xFFB12729);
+
+  /// =============== テーマカラー =====================
+
+  /// 緑
+  static const Color themaGreen = Color(0xFF26b19a);
+
+  /// 紫
+  static const Color themaPurple = Color(0xFF8526b1);
+
+  /// 黄色
+  static const Color themaYellow = Color(0xFFb1ab26);
+
+  /// オレンジ
+  static const Color themaOrange = Color(0xFFb16926);
+
+  /// 黒
+  static const Color themaBlack = Color(0xFF333333);
+
+  /// 灰色
+  static const Color themaGray = Color(0xFF90a2aa);
 }
