5f28bcdf044cc20e5b3f2cca199911efb186fb0f
amefure <shachi10969@gmail.com>
2025-04-04 22:07:11 +0900

    change_app_package_nameの導入とアプリIDの変更

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.salary"
+    namespace = "com.ame.Salary"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.salary"
+        applicationId = "com.ame.Salary"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/ame/Salary/MainActivity.kt
+++ android/app/src/main/kotlin/com/ame/Salary/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.salary
+package com.ame.Salary
 
 import io.flutter.embedding.android.FlutterActivity
 
